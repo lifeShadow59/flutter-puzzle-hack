62885ffbdd5f29312cc77f95edabffa3a691022e
Abhishek Dhanani <abhishek9712a@gmail.com>
2022-01-29 16:55:01 +0530

    project setup

---
+++ lib/routes/routes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_puzzle_hack/main.dart';
+import 'package:flutter_puzzle_hack/routes/routes_name.dart';
+
+class Routes {
+  static Route onGenerateRoute(RouteSettings settings) {
+    // final Object? arguments = settings.arguments;
+
+    Widget screen;
+    switch (settings.name) {
+      case RoutesName.homeRoute:
+        screen = const HomePage();
+        break;
+      default:
+        screen = const Scaffold();
+        break;
+    }
+    return MaterialPageRoute(
+      builder: (BuildContext context) => screen,
+      settings: settings,
+    );
+  }
+}
